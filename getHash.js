13c8ae8279b3bfccfe97650979ac978b23cf7982
builder_247 <14019974+builder-247@users.noreply.github.com>
2024-02-10 10:49:50 +0200

    decompiler test

--- getHash.js
+++ getHash.js
@@ -1,44 +1,67 @@
-const vpk = require("vpk");
-const fs = require("fs");
-const crypto = require("crypto");
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+import { spawn } from 'child_process';
+
+import vpk from 'vpk';
 
 // Dota 2 installation
-const dir = '/SteamLibrary/steamapps/common/dota 2 beta/game/dota'
+const dir = '/mnt/games/SteamLibrary/steamapps/common/dota 2 beta/game/dota';
 
 const vpkDir = new vpk(dir + '/pak01_dir.vpk');
 vpkDir.load();
 
-const vpkIconFiles = vpkDir.files.filter((f) => f.startsWith('panorama/images/econ'));
+let items = {};
+
+const extensionRegex = /_(png|jpg|psd)\.(png|jpeg)$/
 
 // Append this to `https://steamcdn-a.akamaihd.net/apps/570/`
 function itemCDNURL(vpkPath, hash) {
-  const path = vpkPath.replace('panorama/images/', '').replace(/_[\w]{3}\.vtex_c/, '')
-  return `icons/${path}.${hash}.png`;
-}
-
-/*
-* Assumes there is only one PNG file inside the vtex_c file
- */
-function loadPNG(fileBuffer) {
-  const start = fileBuffer.indexOf('89504E47', 0, 'hex'); // 0x89 PNG
-  const end = fileBuffer.indexOf('49454E44', 0, 'hex') + 8; // IEND
-  return fileBuffer.subarray(start, end);
+  const filePath = vpkPath.replace(`${path.resolve('./data')}/panorama/images/`, '').replace(extensionRegex, '')
+  return `icons/${filePath}.${hash}.png`;
 }
 
 function getHash(path) {
-  const PNGBuffer = loadPNG(vpkDir.getFile(path));
+  const PNGBuffer = fs.readFileSync(path);
   const hashSum = crypto.createHash('sha1');
   hashSum.update(PNGBuffer);
   return hashSum.digest('hex');
 }
 
-let items = {};
+async function decompileFiles() {
+  return new Promise((resolve) => {
+    const decompiler = spawn('./bin/Decompiler', ['-i', `${dir}/pak01_dir.vpk`, '-o', './data', '-e', 'vtex_c', '-f', 'panorama/images/econ', '-d'])
+    decompiler.stdout.on('data', async (data) => {
+      // console.log(`Decompiler stdout: ${data}`);
+      const path = data.toString().match(/written to "([^"]+)"/)?.[1];
+      // Compute hashes as files as decompiled
+      if (path !== undefined) {
+        const split = path.split('/');
+        const name = split[split.length - 1].replace(extensionRegex, '');
+        const hash = getHash(path);
+        items[name] = itemCDNURL(path, hash);
+      } else {
+        console.log(`Decompiler stdout: ${data}`);
+      }
+    });
+    decompiler.on('close', resolve)
+  })
+}
 
-vpkIconFiles.forEach((path) => {
-  const split = path.split('/');
-  const name = split[split.length - 1].replace(/_[\w]{3}\.vtex_c/, '');
-  const hash = getHash(path);
-  items[name] = itemCDNURL(path, hash);
-})
+/*
+* Assumes there is only one PNG file inside the vtex_c file
+ */
+async function loadPNG(fileBuffer, path) {
+  const start = fileBuffer.indexOf('89504E47', 0, 'hex'); // 0x89 PNG
+  const end = fileBuffer.indexOf('49454E44', 0, 'hex') + 8; // IEND
+  let PNGBuffer = fileBuffer.subarray(start, end);
+  if (PNGBuffer.length === 0) {
+    // Naive method didn't work, load decompiled file
+    PNGBuffer = fs.readFileSync(`./data/${path.replace('vtex_c', 'png')}`);
+  }
+  return PNGBuffer;
+}
+
+await decompileFiles();
 
 fs.writeFileSync('./build/icons.json', JSON.stringify(items, null, 2));
